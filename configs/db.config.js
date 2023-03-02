dc1852dac03620d9047653974ba139c453e9ad74
Mr-Georgie <george.isiguzo@yahoo.com>
2023-03-02 14:18:48 +0100

    change db config for live env

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,8 +1,8 @@
 const dbConfig = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "password",
-  DB: "testdb",
+  HOST: "mokadb.czh6ps9bsrnb.us-east-1.rds.amazonaws.com",
+  USER: "admin",
+  PASSWORD: "amasaabubakar",
+  DB: "georgie",
   dialect: "mysql",
   pool: {
     max: 5,
@@ -11,19 +11,5 @@ const dbConfig = {
     idle: 10000,
   },
 };
-
-// const dbConfig = {
-//   HOST: "mokadb.czh6ps9bsrnb.us-east-1.rds.amazonaws.com",
-//   USER: "admin",
-//   PASSWORD: "amasaabubakar",
-//   DB: "georgie",
-//   dialect: "mysql",
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000,
-//   },
-// };
 
 export default dbConfig;
